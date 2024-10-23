690978be35014669593dc0a3804bf99094e36d29
itsk2 <codonkellyn3@gmail.com>
2024-10-23 16:17:57 +0800

    Latest

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/dbConfig');
+const auth = require('./routes/auth');
+
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Connect to MongoDB
+connectDB();
+
+
+// Routes
+app.use("/api/v1", auth);
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

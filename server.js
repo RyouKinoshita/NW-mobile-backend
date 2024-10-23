d1184b730d3d43850988dadce71ff24dda6d75b2
Dottyyyyy <orekihoutarou126@gmail.com>
2024-10-23 12:12:16 +0800

    Backend Register Update

---
+++ server.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv');
+// const { Server } = require('socket.io');
+const connectDatabase = require('./config/database');
+const app = require('./App');
+dotenv.config({ path: './config/.env' });
+connectDatabase();
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => console.log(`Server Started: http://localhost:${port}/`))

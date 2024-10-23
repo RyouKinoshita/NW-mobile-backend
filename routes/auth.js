81de88e4bd9c42ab6eba83440d2168da0b36c41a
itsk2 <codonkellyn3@gmail.com>
2024-10-23 15:32:44 +0800

    Latest Update

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../utils/multer");
+
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
+
+
+router.post("/register", upload.single("avatar"), registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
+
+module.exports = router;
